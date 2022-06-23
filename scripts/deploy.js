c141b4498931fc595d6ff9528a32acb009f40422
Sergio Valadez <sergioval@gmail.com>
2022-06-23 18:46:20 -0500

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,17 @@
+const { ethers } = require("hardhat")
+
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners()
+  console.log('Deploying contract with the following address:', deployer.address)
+
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks")
+  const deployed = await PlatziPunks.deploy()
+  console.log('Deployed contract with the following address:', deployed.address)
+
+
+}
+
+deploy().then(() => process.exit(0)).catch(error => {
+  console.error(error)
+  process.exit(1)
+})
